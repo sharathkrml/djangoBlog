f193fdfa4850152787c3bfa74b83035c64734ef0
sharathv <sharathkrml@gmail.com>
2020-08-22 13:54:27 +0530

    3 video completed added dummy data in views loaded static ,urls, added block CONTENT

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
+
+posts = [
+    {
+        'author' : "Sharath.v",
+        'title' : "Blog1",
+        'content' : "my first blog",
+        'date_posted' : 'August 22 2020'
+    },
+    {
+        'author' : "mais",
+        'title' : "Blog2",
+        'content' : "my 2nd blog",
+        'date_posted' : 'may 22 2020'
+    },
+    {
+        'author' : "Sharath.v",
+        'title' : "blog3",
+        'content' : "my 3rd blog",
+        'date_posted' : 'August 21 2020'
+    }
+]
 def home(request):
-    return HttpResponse("hello aorld")
+    context = {
+        'posts' : posts,
+        'title' : 'blog-home'
+    }
+    return render(request,'blog/home.html',context)
 
 def about(request):
-    return HttpResponse("about")
+    return render(request,'blog/about.html')
