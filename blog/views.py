a47214ee010cf1b9f6b3a30f68d2812de55e6760
sharathv <sharathkrml@gmail.com>
2020-08-23 12:14:11 +0530

    5 video completed
    created post model and passed data to website and registered to admin

--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,10 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
-posts = [
-    {
-        'author' : "Sharath.v",
-        'title' : "Blog1",
-        'content' : "my first blog",
-        'date_posted' : 'August 22 2020'
-    },
-    {
-        'author' : "mais",
-        'title' : "Blog2",
-        'content' : "my 2nd blog",
-        'date_posted' : 'may 22 2020'
-    },
-    {
-        'author' : "Sharath.v",
-        'title' : "blog3",
-        'content' : "my 3rd blog",
-        'date_posted' : 'August 21 2020'
-    }
-]
 def home(request):
     context = {
-        'posts' : posts,
-        'title' : 'blog-home'
+        'posts' : Post.objects.all()
     }
     return render(request,'blog/home.html',context)
 
