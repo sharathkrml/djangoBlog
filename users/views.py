f7332fc8076543c6fef23d47cfafc42275330d97
sharathv <sharathkrml@gmail.com>
2020-08-23 16:58:53 +0530

    6.3 completed
    regster page will redirect to login page
    
    logout teomplate added

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.template import RequestContext
 # Create your views here.
 def register(request):
     if request.method=="POST":
@@ -9,7 +10,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request,f'Account is created for {username}!')
-            return redirect('blog-home')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request,'users/register.html',{'form':form})
