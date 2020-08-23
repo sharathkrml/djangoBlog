ed7e5fff60f1fec8b8a43a29bc07f3b39d3cc98f
sharathv <sharathkrml@gmail.com>
2020-08-23 17:20:17 +0530

    6.6 added  profile page
    can accessprofile from adding url

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path,include
-from users.views import register as user_register
+from users import views as user_views
 urlpatterns = [
     path('',include('blog.urls')),
     path('admin/', admin.site.urls),
-    path('register/',user_register,name="register"),
+    path('register/',user_views.register,name="register"),
+    path('profile/',user_views.profile,name="profile"),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name="login"),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name="logout")
 ]
