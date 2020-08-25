c7eb58e17c933bdc6c0badae57a5a3257b96a4c4
sharathv <sharathkrml@gmail.com>
2020-08-25 20:11:34 +0530

    9.3.5 video complted
    detailview added
    home.html added route to detail_view

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 from .views import PostListView, PostDetailView
 urlpatterns = [
     path('',PostListView.as_view(),name="blog-home"),
-    path('post/<int:pk>/',PostDetailView.as_view(),name="blog-home"),
+    path('post/<int:pk>/',PostDetailView.as_view(),name="post-detail"),
     path('about/',views.about,name="blog-about"),
 ]
