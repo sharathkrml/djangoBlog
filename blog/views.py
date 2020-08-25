1613132d07a8919974f8c3cc3de4ab7fd924e306
sharathv <sharathkrml@gmail.com>
2020-08-25 21:54:21 +0530

    9.4 completed
    added PostCreate and forms
    have integrity error
    not null contraint failed blog_post.author_id

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView,DetailView
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView
+)
 # Create your views here.
 
 def home(request):
@@ -18,7 +22,9 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
    
-   
+class PostCreateView(CreateView):
+    model = Post   
+    fields = ['title','content']
    
 def about(request):
     return render(request,'blog/about.html')
