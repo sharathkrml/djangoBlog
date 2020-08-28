5d8d4a9bebdea1ca132b06bc45ab6aa43465e451
sharathv <sharathkrml@gmail.com>
2020-08-28 12:48:15 +0530

    added hostpasswd

--- django_project/settings.py
+++ django_project/settings.py
@@ -138,6 +138,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'sharathkrml@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = 'wvbjengfxbhfppwn'
 
 django_heroku.settings(locals())
