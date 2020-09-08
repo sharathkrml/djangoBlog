92428cab4438d450ab957f97f053e293c8f969ba
sharathv <sharathkrml@gmail.com>
2020-09-08 12:17:34 +0530

    added API to view specific post also

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 
 from django.urls import path
-from .views import posts_list
+from .views import posts_list,post_detail
 urlpatterns = [
-    path('posts/',posts_list)
+    path('posts/',posts_list),
+    path('posts/<int:id>/',post_detail)
 ]
--- api/views.py
+++ api/views.py
@@ -1,11 +1,11 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from django.http import HttpResponse
 from blog.models import Post
 from .serializers import PostSerializer
 
-@api_view(['GET','PUT','DELETE'])
+@api_view(['GET','POST'])
 def posts_list(request):
         try:
             posts = Post.objects.all()
@@ -14,6 +14,22 @@ def posts_list(request):
         if request.method == 'GET':
             serializer = PostSerializer(posts,many=True)
             return Response(serializer.data)
+        elif request.method == 'POST':
+            serializer = PostSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data,status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT','DELETE'])
+def post_detail(request,id):
+        try:
+            posts = Post.objects.get(id=id)
+        except Post.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+        if request.method == 'GET':
+            serializer = PostSerializer(posts)
+            return Response(serializer.data)
         elif request.method == 'PUT':
             serializer = PostSerializer(posts, data=request.data)
             if serializer.is_valid():
@@ -21,5 +37,5 @@ def posts_list(request):
                 return Response(serializer.data,status=201)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         elif request.method == 'DELETE':
-            Post.delete()
+            posts.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
