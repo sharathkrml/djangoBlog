7f538b414f9c65481ec9c5eb974a8fbf792a7f39
E-XRAy <72224721+E-XRAy@users.noreply.github.com>
2020-10-20 23:31:34 +0530

    settings revised for local setup

--- django_project/settings.py
+++ django_project/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'o_2ul8oad3@tv7#x415ft6n$$gslmzp^bn$*18$sc6l0v@b+&0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['djngoblgapln.herokuapp.com']
+ALLOWED_HOSTS = ['djngoblgapln.herokuapp.com','localhost']
 
 
 # Application definition
